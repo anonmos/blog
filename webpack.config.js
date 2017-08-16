85e35aeed4bf8e50aaaa9739ffe862ba21672b00
Tim Growney <moscro@gmail.com>
2017-08-15 21:15:00 -0700

    Add rudimentary minification.

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,5 @@
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
-const path = require('path');
-const webpack = require('webpack');
-
-module.exports = {
-  devtool: 'inline-source-map',
-  entry: './js/index.ts',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build/js')
-  },
-  resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
-  },
-  module: {
-    loaders: [ // loaders will work with webpack 1 or 2; but will be renamed "rules" in future
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/,
-        exclude: /node_modules/,
-        loader: 'ts-loader'
-      }
-
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(['build']),
-    new CopyWebpackPlugin([{from: 'static', to: '../'}]),
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      popper: "popper.js",
-      bootstrap: "bootstrap"
-    })
-  ]
+module.exports = function(env) {
+  if (env) {
+    return require(`./webpack.${env}.js`)
+  }
 }
